a7c02471e761000d92971cdf42ac2fb1cff32150
Oghenemarho Orukele <orukele.dev@gmail.com>
2025-11-16 13:28:38 +0100

    batman

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Graphics Programming
+A repository for learning graphics programming with OpenGL.
+
+At the moment, I have been able to achieve the following:
+
+- Install and setup OpenGL, GLFW & GLAD to work with CLion on MacOS
+- 
---
+++ src/1.getting_started/hello_window.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    // glfw: initialise and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
+    if (window == nullptr) // if window is not created, return error message
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
+// glfw: callback function for whenever the window size is changed
+// ---------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions.
+    glViewport(0, 0, width, height);
+}
